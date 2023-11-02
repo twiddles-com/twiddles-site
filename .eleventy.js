575088f8a7da473c391680ed86ea8f493af6345d
James Nash <cirrus@twiddles.com>
2023-11-02 13:32:35 +0000

    fixed asset copying

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy({ "src/assets/**": "/" });
+  eleventyConfig.addPassthroughCopy({"src/assets/": "/"});
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.setServerOptions({
     watch: ["dist/styles.css"]
